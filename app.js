ba5481204281d293d359ba02d06f777a3c916b5a
Aymeric <aymeric.gioia@hotmail.fr>
2017-04-20 16:58:28 +0200

    exo 1

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var app = new Vue({
+Vue.component('mon-composant', {
+	template: '<p>Mon premier composant !</p>'
+});
+new Vue({
 	el: '#app',
 	data: {
 		message: 'Hello World !',
 		'list':['yepco', 'chiko', 'anissa', 'dog', 'lunette'],
 		pur: '<h1>Shooting star</h1>',
+		seen: true
+
 	}
-	Vue.component('todo-item', {
-		template: '<li>Ceci est une liste</li>'
-	})
 })
